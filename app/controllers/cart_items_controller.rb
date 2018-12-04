49bfd5026251de8f3ba847fbdd9589c06d3591b3
escray <zescray@gmail.com>
2018-12-04 18:23:38 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,4 +11,18 @@ class CartItemsController < ApplicationController
     flash[:warning] = "成功将 #{@book.title} 从借书单删除"
     redirect_to carts_path
   end
+
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(book_id: params[:id])
+    @cart_item.update(cart_item_params)
+
+    redirect_to carts_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
