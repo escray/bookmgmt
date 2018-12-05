19be67f0c673d6ba0a517d122172517ffd4ec57f
escray <zescray@gmail.com>
2018-12-05 20:37:00 +0800

    refactor render_cart_total to carts_helper

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,9 @@
 module CartsHelper
+  def render_cart_total(cart)
+    sum = 0
+    cart.cart_items.each do |cart_item|
+      sum += cart_item.quantity if cart_item.quantity.present?
+    end
+    sum
+  end
 end
--- app/models/borrow_list.rb
+++ app/models/borrow_list.rb
@@ -1,3 +1,4 @@
 class BorrowList < ApplicationRecord
   belongs_to :order
+  belongs_to :book
 end
