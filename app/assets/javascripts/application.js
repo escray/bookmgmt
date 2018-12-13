793be2cda0e0f78af21ddffc75635ef11dff54ef
escray <zescray@gmail.com>
2018-12-13 12:00:02 +0800

    yarn install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require rails-ujs
+//= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require bootstrap-sprockets
